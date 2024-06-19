301c2d621004f39d8b6b5d0f6a9c8fa3c4fb3621
Dovi Joel <dovi@joel.family>
2024-06-19 15:10:57 +0200

    initial code commit

---
+++ src/main/java/joel/dovi/sanlam/SanlamApplication.java
@@ -0,0 +1,13 @@
+package joel.dovi.sanlam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanlamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanlamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/joel/dovi/sanlam/SanlamApplicationTests.java
@@ -0,0 +1,13 @@
+package joel.dovi.sanlam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanlamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
